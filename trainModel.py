2fa8f617f948831c4f5ed6ccc9cbeb9e22b5201f
LanceHuang <lancehuang245@outlook.com>
2024-10-25 10:24:10 +0800

    mod: 优化参数，提升模型预测准确率

--- trainModel.py
+++ trainModel.py
@@ -58,7 +58,7 @@ model = ImageGuard()
 criterion = nn.CrossEntropyLoss()
 
 # 优化器
-optimizer = optim.RMSprop(model.parameters(), lr=0.001)
+optimizer = optim.RMSprop(model.parameters(), lr=0.0001)
 
 # 训练模型
 def train_model(model, criterion, optimizer, num_epochs):
@@ -104,27 +104,28 @@ def train_model(model, criterion, optimizer, num_epochs):
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 
-model = train_model(model, criterion, optimizer, num_epochs=50)
+# 训练20轮
+model = train_model(model, criterion, optimizer, num_epochs=20)
 torch.save(model.state_dict(), 'model/image_guard_v1.pth')
 print("Model saved.")
 
-# 图像预测
-def predict_image(image_path, model, class_names):
-    model.eval()
-    img = Image.open(image_path)
-    transform = transforms.Compose([
-        transforms.Resize((299, 299)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-    img = transform(img).unsqueeze(0)
-    img = img.to(device)
-
-    with torch.no_grad():
-        outputs = model(img)
-        _, predicted = torch.max(outputs, 1)
-
-    print(f'预测结果: {class_names[predicted[0]]}')
-
-model.load_state_dict(torch.load('model/image_guard_v1.pth', weights_only=True))
-predict_image('data/validation/neutral/[www.google.com][1989].jpg', model, class_names)
+# 图像测试预测
+# def predict_image(image_path, model, class_names):
+#     model.eval()
+#     img = Image.open(image_path)
+#     transform = transforms.Compose([
+#         transforms.Resize((299, 299)),
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#     ])
+#     img = transform(img).unsqueeze(0)
+#     img = img.to(device)
+#
+#     with torch.no_grad():
+#         outputs = model(img)
+#         _, predicted = torch.max(outputs, 1)
+#
+#     print(f'预测结果: {class_names[predicted[0]]}')
+#
+# model.load_state_dict(torch.load('model/image_guard_v1.pth', weights_only=True))
+# predict_image('data/validation/porn/[www.google.com][10382].jpg', model, class_names)
